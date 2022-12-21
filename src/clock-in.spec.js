781d3872e5c1d43a00dae6c6095cb2ecf0a8f44d
Manel Fernandez <manelfera.dev@outlook.com>
2022-12-21 23:32:32 +0100

    [REF] Cleanup project and test files

--- src/clock-in.spec.js
+++ src/clock-in.spec.js
@@ -1,36 +1,27 @@
 import assert from 'assert';
-
-const gpsIsAvailable = new Promise((resolve, reject) => {
-  resolve(availableGpsCoordinates);
-});
-
-const gpsIsNotAvailable = new Promise((resolve, reject) => {
-  reject();
-});
+import { sendClockIn } from './send-clockin';
 
 describe('time tracking', () => {
-  context('GPS is required', () => {
-    it('sends clock-in when GPS is available', () =>
-      sendClockIn(gpsIsAvailable)
-    );
 
-    it('sends clock-in with coordinates when GPS is available', (done) => {
+  context('Only time tracking', () => {
+
+    it ('should track time', (done) => {
+      sendClockIn(1).then(function(res) {
+        assert(res === "OK");
+        done();
+      });
     });
 
-    it('does NOT send clock-in when no GPS is available', (done) => {
-      sendClockIn(gpsIsNotAvailable)
-        .then(() => assert(false, 'Promise should have been rejected'))
-        .catch(done);
+    it ('try track time but error', (done) => {
+      sendClockIn().then(() => {
+        assert(false);
+      }).catch(function(res) {
+        assert(res === "KO");
+        done();
+      });
     });
 
-    it('warns the user when no GPS is available', () => {
-
-    });
   });
 
-  context('GPS is optional', () => {
-    it('does NOT send GPS data when no GPS is available', () => {
-
-    });
-  });
 });
+
